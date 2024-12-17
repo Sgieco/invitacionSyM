83bd693102d28fddd405b290f3306d75a38403dd
Sgieco <sofiagieco@gmail.com>
2024-12-17 14:57:22 -0300

    Primera versión del proyecto

---
+++ src/main/java/com/invitacion/SyM/SyMApplication.java
@@ -0,0 +1,13 @@
+package com.invitacion.SyM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invitacion/SyM/SyMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invitacion.SyM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
